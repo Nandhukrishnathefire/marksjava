25cd8e4ec62fc93703ead86e063aa163bd5936ef
exam128 <exam128@CSCENTER.COM>
2026-03-12 14:41:14 +0530

    first commit

---
+++ marks.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Studentgrade {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double total;
+
+        System.out.print("Enter english marks: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter tamil marks: ");
+        double b = sc.nextDouble();
+         
+        System.out.print("Enter computer science number: ");
+        double c = sc.nextDouble();
+        
+        System.out.print("Enter life skill marks: ");
+        double d = sc.nextDouble();
+        
+        System.out.print("Enter maths marks: ");
+        double e = sc.nextDouble();
+
+        System.out.println("total: " + (a+b+c+d+e));
+        System.out.println("average: " + ((a+b+c+d+e)/5));
+        System.out.println("pass");
+       }
+
+    }
